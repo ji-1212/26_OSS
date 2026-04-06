8ec8544037dd0c950381ead9719861aebeb7e991
ji_1212 <jihyunpark@sookmyung.ac.kr>
2026-04-06 11:37:40 +0900

    folder upload test by JiHyun

---
+++ data/Test_code.py
@@ -0,0 +1,82 @@
+#This is test in 20260406
+
+#2557
+print("Hello World!")
+
+#1000
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+
+#1001
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a-b)
+
+#10998
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a*b)
+
+#1008
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a/b)
+
+#10869
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#10926
+print(input()+"??!")
+
+#18108
+a = int(input())
+print(a-543)
+
+#10430
+a,b,c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+print((a+b)%c)
+print(((a%c) + (b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+#2588
+a,b,c = input().split()
+d,e,f = input().split()
+print(int(a+b+c)*int(f))
+print(int(a+b+c)*int(e))
+print(int(a+b+c)*int(d))
+
+#11382
+a,b,c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+print(a+b+c)
+
+#10171
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+#10172
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\\__|")
